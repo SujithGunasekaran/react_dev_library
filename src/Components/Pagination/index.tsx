e858074fffe9d51e84db2f38851e723b6e4e7f57
Sujith Gunasekaran <mydevelopstar@gmail.com>
2022-04-25 14:30:02 +0530

    cleanup

--- src/Components/Pagination/index.tsx
+++ src/Components/Pagination/index.tsx
@@ -7,14 +7,6 @@ interface PaginationProps {
     selectedPage: (pageNumber: number) => void
 }
 
-interface PaginationState {
-    totalPageNumber: number,
-    totalPageSets: number,
-    currentPageNumber: number,
-    currentItem: number,
-    numberPerPage: number
-}
-
 
 const Pagination: React.FC<PaginationProps> = (props) => {
 
@@ -94,17 +86,27 @@ const Pagination: React.FC<PaginationProps> = (props) => {
         <React.Fragment>
             <div className='data_table_header_pagination_container'>
                 <div className={`data_table_header_pagination_icon ${currentPageNumber === 1 ? 'disabled' : ''}`} onClick={() => previousPageSet()}>
-                    <i className="bi bi-chevron-double-left icon"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-chevron-double-left icon`} viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                        <path fillRule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                    </svg>
                 </div>
                 <div className={`data_table_header_pagination_icon ${currentItem === 1 ? 'disabled' : ''}`} onClick={() => previousPage()}>
-                    <i className="bi bi-chevron-left icon"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-chevron-left icon`} viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                    </svg>
                 </div>
                 {pageItem ? pageItem : ''}
                 <div className={`data_table_header_pagination_icon ${currentItem === totalPageNumber ? 'disabled' : ''}`} onClick={() => nextPage()}>
-                    <i className="bi bi-chevron-right icon"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-chevron-right icon`} viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
                 </div>
                 <div className={`data_table_header_pagination_icon ${currentPageNumber === totalPageSets ? 'disabled' : ''}`} onClick={() => nextPageSet()}>
-                    <i className="bi bi-chevron-double-right icon"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-chevron-double-right icon`} viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z" />
+                        <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
                 </div>
             </div>
         </React.Fragment>
